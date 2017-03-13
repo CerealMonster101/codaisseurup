e743e4c560859467dde04843070a0c8b78ecca00
VANESSA <VANESSA.LG.HO@GMAIL.COM>
2017-03-13 15:00:45 +0100

    Update Devise Views

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,10 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Codaisseurup
+module Codaisseurbnb
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    console do
+      ActiveRecord::Base.connection
+    end
   end
 end
