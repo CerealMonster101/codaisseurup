c6050a5b7d2630014fb25837c24dccb093d17bfe
VANESSA <VANESSA.LG.HO@GMAIL.COM>
2017-03-20 16:31:44 +0100

    Add API events show and create

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -4,4 +4,36 @@ class Api::EventsController < ApplicationController
       events: Event.all
     }.to_json
   end
+
+
+  def show
+    event = Event.find(params[:id])
+    render status: 200, json: {
+      event: event
+    }. to_json
+  end
+
+  def create
+    event = Event.new(event_params)
+
+    if event.save
+      render status: 201, json: {
+        message: "Event successfully created",
+        event: event
+      }. to_json
+    else
+      render status: 422, json: {
+        errors: event.errors
+      }.to_json
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :includes_food, :includes_drinks, :price, :starts_at, :ends_at, :capacity, :active, category_ids: [])
+  end
+end
+
+
 end
