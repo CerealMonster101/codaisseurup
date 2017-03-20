43c3c49f5ec818807c9f90fb3618525ab0636c84
VANESSA <VANESSA.LG.HO@GMAIL.COM>
2017-03-20 21:59:01 +0100

    Through Association: Views

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,9 @@ class RegistrationsController < ApplicationController
 before_action :authenticate_user!
 
   def create
-    @registrations = current_user.registrations.create(registration_params)
+    @registrations = current_user.registrations.create(registration_params.merge(room_id: params[:room_id]))
+    @registration.set_to_total_price
+    @registration.save
 
     redirect_to_@registration.event, notice: "Thank you for registering!"
   end
@@ -10,6 +12,6 @@ before_action :authenticate_user!
   private
 
   def registration_params
-    params.require(:registration).permit(:starts_at, :ends_at, :price, :total, :event_id)
+    params.require(:registration).permit(:starts_at, :ends_at)
   end
 end
